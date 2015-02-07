15c67940b1a6c2385bf17ef76590623f8eb91f4e
Dave Hakim <dhakim@precociouspeanut.com>
2015-02-07 18:26:54 -0500

    Initial Commit

---
+++ InliningTableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InliningTableViewDemo
+//
+//  Created by David Hakim on 2/7/15.
+//  Copyright (c) 2015 Precocious Peanut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ InliningTableViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InliningTableViewDemo
+//
+//  Created by David Hakim on 2/7/15.
+//  Copyright (c) 2015 Precocious Peanut. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
